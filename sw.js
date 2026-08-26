ed795312c3ef55489b99f27ac2ddee0c6e3e20d8
JordanC1128 <jordan.crites1128@gmail.com>
2026-08-26 01:00:33 -0400

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='plate-loader-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
